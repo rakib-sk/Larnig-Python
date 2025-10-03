7372860f8ec27987dba354d689185984148c935b
rakib-sk <rh01828315879>
2025-10-03 09:17:07 +0600

    add 2 pattern file

---
+++ pattern/pratice-pattern.py
@@ -0,0 +1,25 @@
+# ****
+# ****
+# ****
+#Print this pattern
+n = 3
+for i in range(n):
+	for j in range(n):
+		print("* ", end = " ")
+	print()
+#
+#
+#******
+#******
+#******
+#Print it
+print("\n----Task2--------")
+o= 3
+m = 6  
+for i in range(o):
+	for j in range(m):
+		print("*", end = " ")
+	print()
+#
+#
+#
---
+++ pattern/shape.py
@@ -0,0 +1,13 @@
+#Tringle shape drawing 
+print("-------Tringle-------")
+n = 5
+for i in range(1,n+1):
+	print("* " * i)
+#
+#Square shape drawing 
+print("\n-------Square-------")
+n = 5
+for i in range(n):
+	for j in range(n):
+		print("*", end = " ")
+	print()
